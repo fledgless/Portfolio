cf6c09bb5e6961b15d61a10746c86e09afbe4b8a
Cameron Cardinal <cameriancardinal@gmail.com>
2023-11-27 14:26:45 +0100

    changes271123

--- index.php
+++ index.php
@@ -55,9 +55,9 @@
             <h2>A-propos</h2>
             <hr width="1" size="500"/>
             <div id="paragraphe">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic commodi in temporibus non rem! Quod impedit vero voluptate perferendis repudiandae molestias ut tenetur, saepe, fuga a magni cumque debitis repellat.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic commodi in temporibus non rem! Quod impedit vero voluptate perferendis repudiandae molestias ut tenetur, saepe, fuga a magni cumque debitis repellat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus natus nulla id totam commodi voluptates dignissimos rerum molestias officiis harum molestiae at accusamus dicta ad ex fuga, tempore facilis nemo.</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis dolores quasi nam asperiores! Modi optio a hic provident reiciendis inventore ipsa quisquam non, quos temporibus quam saepe possimus sed veritatis?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maxime tempore eveniet sit architecto consectetur dolorem? Quibusdam, quod facere? Eius architecto sint in mollitia officiis quidem dolorum similique enim nesciunt!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis dolore, praesentium cum quidem sed tenetur repellendus quam nostrum, a ipsum sit nesciunt neque perspiciatis, incidunt ad ex placeat accusantium impedit! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maxime tempore eveniet sit architecto consectetur dolorem? Quibusdam, quod facere? Eius architecto sint in mollitia officiis quidem dolorum similique enim nesciunt!</p>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam asperiores rem dolore blanditiis quis. Modi earum odio neque adipisci! Labore perspiciatis enim temporibus similique expedita aperiam atque omnis impedit corporis.</p>
             </div>
         </section>
@@ -66,35 +66,43 @@
             <hr width="1" size="500"/>
             <div id="experience">
                 <h3>Expérience n°1</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Sunt exercitationem omnis iste excepturi, similique voluptatibus eius itaque!</p>   
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt, excepturi dolore deleniti molestiae id sit consequuntur maxime inventore ullam recusandae amet atque laborum tempore vel, et commodi, quaerat temporibus.</p>   
                 <!-- <hr width="900" size="1"/> -->
                 <h3>Expérience n°2</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Sunt exercitationem omnis iste excepturi, similique voluptatibus eius itaque!</p>    
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt, excepturi dolore deleniti molestiae id sit consequuntur maxime inventore ullam recusandae amet atque laborum tempore vel, et commodi, quaerat temporibus.</p>    
                 <!-- <hr width="900" size="1"/> -->
                 <h3>Expérience n°3</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Sunt exercitationem omnis iste excepturi, similique voluptatibus eius itaque!</p>  
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime placeat eos alias maiores quae, necessitatibus, doloribus labore eligendi consectetur perspiciatis voluptatum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deserunt, excepturi dolore deleniti molestiae id sit consequuntur maxime inventore ullam recusandae amet atque laborum tempore vel, et commodi, quaerat temporibus.</p>  
             </div>
         </section>
         <section id="projets">
             <h2>Projets</h2>
-            <hr width="500" size="1"/>
+            <hr width="500" size="2"/>
             <div id="projet">
-                <div id="projet1">
-                    <img src="Portfolio.png" alt="Image projet 1">
+                <div class="projet">
+                    <img src="Portfolio.png" alt="Image projet 1 ">
                     <h3><a target="_blank" href="https://github.com/fledgless/Portfolio">Ce portfolio</a></h3>
                 </div>
-                <div id="projet2">
+                <div class="projet">
                     <img src="#" alt="Image projet 2">
                     <h3>Projet n°2</h3>
                 </div>    
-                <div id="projet3">
+                <div class="projet">
                     <img src="#" alt="Image projet 3">
-                    <h3>Projet °3</h3>
+                    <h3>Projet n°3</h3>
+                </div>
+                <div class="projet">
+                    <img src="#" alt="Image projet 4">
+                    <h3>Projet n°4</h3>
+                </div>
+                <div class="projet">
+                    <img src="#" alt="Image projet 5">
+                    <h3>Projet n°5</h3>
                 </div>
             </section>
         <section id="contact">
             <h2>Contact</h2>
-            <hr width="500" size="1"/>
+            <hr width="500" size="2"/>
             <div class="form"> 
                 <div id="error_message"></div> 
                 <form action="" 
@@ -120,9 +128,7 @@
                                 id="message"> 
                         </textarea> 
                     </div> 
-                    <div class="btn"> 
-                        <input type="submit"> 
-                    </div> 
+                    <button>Envoyer</button>
                 </form> 
             </div> 
         </section>
