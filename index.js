9a8074dace4a9f7e0a3d5ece8043c6140aa3164e
Cameron Cardinal <cameriancardinal@gmail.com>
2023-11-28 08:41:51 +0100

    changes

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let nav = document.querySelector("nav");
 let hamburger =  document.querySelector("#hamburger");
 hamburger.addEventListener("click", () => {
         nav.classList.toggle("open");
-        
 });
 
 // toggle nav sur liens
@@ -28,24 +27,23 @@ function validate() {
 
     // vérification nom
     if (name.length < 3) { 
-        errors.push("Veuillez entrer un nom valide");
+        errors.push("Veuillez entrer un nom valide.");
     } 
     // vérification sujet
     if (subject.length < 5) { 
-        errors.push("Veuillez entrer un sujet valide");
+        errors.push("Veuillez entrer un sujet valide.");
     }
     // vérification du mail
     if (email.indexOf("@") == -1 || email.length < 6) { 
-        errors.push("Veuillez entrer une adresse mail valide");
+        errors.push("Veuillez entrer une adresse mail valide.");
     } 
     // vérification du message
     if (message.length <= 30) { 
-        errors.push("Veuillez écrire un message de plus de 30 caractères");
+        errors.push("Veuillez écrire un message de plus de 30 caractères.");
     } 
     // envoi du message en fonction des erreurs
     if (errors.length > 0) { 
-        error_message.innerHTML = 
-            errors.join("<br>"); 
+        error_message.textContent = errors.join(" "); 
         return false;} 
     else { 
         alert( 
